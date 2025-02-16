ec4f7e012ceedd90a05da103e49d2b421dd6f434
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-02-16 17:07:08 +0530

    src: generate the red sphere -> camera test

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 use ray::Ray;
-use vec3::{unit_vector, Vec3};
+use vec3::{dot, unit_vector, Vec3};
 
 mod ray;
 mod vec3;
 
-//approximate the pixel centers
+//hit the sphere to get a basic camera running
+fn hit_sphere(center: Vec3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = dot(&r.direction(), &r.direction());
+    let b = 2.0 * dot(&oc, &r.direction());
+    let c = dot(&oc, &oc) - radius * radius;
+    let discriminant = b * b - (4.0 * a * c);
+    discriminant > 0.0
+}
 
-fn color(r: Ray) -> Vec3 {
+// Fixed color function
+fn color(r: &Ray) -> Vec3 {
+    if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Vec3::new(1.0, 0.0, 0.0);
+    }
     let unit_direction = unit_vector(&r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0);
+    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
 }
 
 fn main() {
@@ -33,7 +45,7 @@ fn main() {
             let u = i as f64 / nx as f64;
             let v = j as f64 / ny as f64;
             let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
-            let col = color(r);
+            let col = color(&r);
 
             let ir = (255.99 * col.x()) as i64;
             let ig = (255.99 * col.y()) as i64;
