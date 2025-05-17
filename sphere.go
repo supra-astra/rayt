1466ccded31b3f48620877ecf0ca32692d8565e2
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-05-17 17:14:21 +0530

    feat: added sphere hit

---
+++ sphere.go
@@ -0,0 +1,43 @@
+package main
+
+import "math"
+
+type Sphere struct {
+	Center Point3
+	Radius float64
+}
+
+func (s *Sphere) Hit(r *Ray, tMin, tMax float64) (bool, HitRecord) {
+	oc := VecSub(&r.Orig, &s.Center)
+	a := DotProduct(&r.Dir, &r.Dir)
+	halfB := DotProduct(&r.Dir, oc)
+	c := DotProduct(oc, oc) - s.Radius*s.Radius
+
+	discriminant := halfB*halfB - a*c
+
+	if discriminant < 0 {
+		return false, HitRecord{}
+	}
+
+	sqrtD := math.Sqrt(discriminant)
+
+	//nearest root within the acceptable range
+	root := (-halfB - sqrtD) / a
+	if root < tMin || root > tMax {
+		root = (-halfB + sqrtD) / a
+		if root < tMin || root > tMax {
+			return false, HitRecord{}
+		}
+	}
+
+	p := r.At(root)
+	normal := ScalarDiv(VecSub(&p, &s.Center), s.Radius)
+
+	rec := HitRecord{
+		P:      p,
+		Normal: *normal,
+		T:      root,
+	}
+
+	return true, rec
+}
